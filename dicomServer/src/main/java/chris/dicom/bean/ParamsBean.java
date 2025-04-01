4de35af8cdf88104e1a8a3d353018a47211c583f
chris.ceo <chris.ceo@gmail.com>
2025-04-01 14:58:35 +0530

    Version 1

---
+++ dicomServer/src/main/java/chris/dicom/bean/ParamsBean.java
@@ -0,0 +1,34 @@
+package chris.dicom.bean;
+
+public class ParamsBean {
+	String ben_no;
+	String community;
+	String ownership;
+
+	public String getBen_no() {
+		return ben_no;
+	}
+
+	public void setBen_no(String ben_no) {
+		this.ben_no = ben_no;
+	}
+
+	public String getCommunity() {
+		return community;
+	}
+
+	public void setCommunity(String community) {
+		this.community = community;
+	}
+
+	public String getOwnership() {
+		return ownership;
+	}
+
+	public void setOwnership(String ownership) {
+		this.ownership = ownership;
+	}
+	
+	
+
+}
---
+++ dicomServer/src/main/java/chris/dicom/util/TomoConstants.java
@@ -0,0 +1,6 @@
+package chris.dicom.util;
+
+public class TomoConstants {
+	public static final String STORE_INSTANCE="http://localhost:2305/Upload/StoreSCP";
+
+}
